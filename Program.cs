22e833bf762d9ff201d83477442dcbde4f3d775d
dinhtai-02 <dinhtai30602@gmail.com>
2025-02-20 15:11:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+
+//namespace MyApplication
+//{
+//    class Program
+//    {
+//        static void MyMethod()
+//        {
+
+//        }
+//    }
+//}
+
+int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+foreach (int i in numbers)
+{
+    Console.WriteLine(i);
+}
