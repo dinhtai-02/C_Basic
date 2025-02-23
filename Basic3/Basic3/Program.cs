8b6571702ab57dfcbf3b544aedda1093bda1a53c
dinhtai-02 <dinhtai30602@gmail.com>
2025-02-23 23:05:47 +0700

    First commit

---
+++ Basic3/Basic3/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+/*namespace MyTest
+{
+    class Vehicle
+    {
+        public string brand = "Ford";
+        public void honk()
+        {
+            Console.WriteLine("Tuut, Tuut!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string modelName = "Mustang";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.honk();
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+        }
+    }
+}*/
+
+/*namespace MyTest
+{
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal make a sound");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The cat says: meow meow");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myCat = new Cat();
+            Animal myDog = new Dog();
+
+            myAnimal.animalSound();
+            myCat.animalSound();
+            myDog.animalSound();
+        }
+    }
+}*/
+
